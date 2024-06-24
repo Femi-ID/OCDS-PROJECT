54874ee11780be29bc6798b84b6c53a2dc4f6fde
Femi-ID <104504319+Femi-ID@users.noreply.github.com>
2024-06-25 00:51:32 +0100

    Edit (4) settings config for deployment.

--- ocds/settings.py
+++ ocds/settings.py
@@ -96,22 +96,22 @@ WSGI_APPLICATION = 'ocds.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#         'USER': '',
-#         'PASSWORD': '',
-#         'HOST': '', 
-#         'PORT': '',
-#     },
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '', 
+        'PORT': '',
+    },
 
-#     'default': dj_database_url.config(
-#         # Replace this value with your local database's connection string.
-#         default='postgresql://postgres:postgres@localhost:5432/mysite',
-#         conn_max_age=600
-#     )
-# }
+    # 'default': dj_database_url.config(
+    #     # Replace this value with your local database's connection string.
+    #     default='postgresql://postgres:postgres@localhost:5432/mysite',
+    #     conn_max_age=600
+    # )
+}
 
 # postgres://USER:PASSWORD@INTERNAL_HOST:PORT/DATABASE
 database_url = config("DATABASE_URL")
